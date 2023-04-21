b9e69895857158402b46db32998aea07c9a47711
jcniabemr <john.connell@niab.com>
2023-04-21 23:37:33 +0100

    update

--- AoC_2022.py
+++ AoC_2022.py
@@ -4,40 +4,95 @@
 ####Import functions 
 import sys 
 
-####Day 1 (Part1) Calorie counting 
+##############################
+#                            #
+#   Day 1 calorie counting   #
+#   ######################   #
+#                            #
+##############################
 ####Open and split data 
-#data=open(sys.argv[1]).read().split('\n\n')
+data=open(sys.argv[1]).read().split('\n\n')
+
 ####Transform string to a list of integers 
-#int_list=[tuple(map(int, x.split())) for x in data]
+int_list=[tuple(map(int, x.split())) for x in data]
+
 ####Find the highest value of the list of tuples
-#highest_val=max(map(sum, int_list)) 
-#print('The highest single sum value is', highest_val)
-####Day 1 (Part2) 
+highest_val=max(map(sum, int_list)) 
+print(highest_val)
+
+####(Part2) 
 ####Order intlit by sum of values descending 
-#int_list.sort(key=sum, reverse=True)
-#largest_val_3=sum(map(sum, int_list[:3]))
-#print('The sum of the sum of the largest three is', largest_val_3)
+int_list.sort(key=sum, reverse=True)
+largest_val_3=sum(map(sum, int_list[:3]))
+print(largest_val_3)
+
+#################################
+#                               #
+#   Day 2 rock paper scissors   #
+#   #########################   #
+#                               #
+#################################
+
+####Open and split data 
+data=open(sys.argv[1]).read().strip().split('\n')
+
+###Dict of possible results
+poss_results={"A X":4, "A Y":8, "A Z":3, "B X":1, "B Y":5, "B Z":9, "C X":7, "C Y":2, "C Z":6}
+
+####Add up points 
+points=0
+for x in data:
+	points+=poss_results[x]
+
+print (points)
+
+####(Part2)
+####Manipulate winning and losing 
+man_poss_results={"A X":3, "A Y":4, "A Z":8, "B X":1, "B Y":5, "B Z":9, "C X":2, "C Y":6, "C Z":7}
+
+####Add up points 
+points_2=0
+for x in data:
+	points_2+=man_poss_results[x]
+
+print(points_2)
+
+#####################################
+#                                   #
+#   Day 3 rucksack reorganisation   #
+#   #############################   #
+#                                   #
+#####################################
+from string import ascii_letters
+
+####Open and split data 
+data=open(sys.argv[1]).read().strip().split("\n")
+
+#Find length of half rucksack and set of unique characters in the left and right side 
+count=0
+for x in data:
+	y=len(x)//2
+	leftside=set(x[:y])
+	rightside=set(x[y:])
+#	print (x, leftside, rightside)
+	for n,l in enumerate(ascii_letters):
+		if l in leftside and l in rightside:
+			count+=n+1
+
+print (count)
+
+####(Part2)
+####Iterate through rucksacks in threes and find common character 
+count=0
+end=3
+for x in range(0, len(data),3):
+	r=data[x:end]
+	end+=3
+	#print (r)
+	for n,l in enumerate(ascii_letters):
+		if l in r[0] and l in r[1] and l in r[2]:
+			count+=n+1
+
+print (count) 
 
 
-###Day2 (Part1) Rock paper scissors 
-SCORES = (
-    1 + 3, # A (rock)     vs X (rock)     -> draw
-    2 + 6, # A (rock)     vs Y (paper)    -> win
-    3 + 0, # A (rock)     vs Z (scissors) -> loss
-    1 + 0, # B (paper)    vs X (rock)     -> loss
-    2 + 3, # B (paper)    vs Y (paper)    -> draw
-    3 + 6, # B (paper)    vs Z (scissors) -> win
-    1 + 6, # C (scissors) vs X (rock)     -> win
-    2 + 0, # C (scissors) vs Y (paper)    -> loss
-    3 + 3, # C (scissors) vs Z (scissors) -> draw
-)
-
-table=str.maketrans('ABCXYZ', '036123')
-
-data=open(sys.argv[1]).read().translate(table)
-
-for x in data.splitlines():
-	a,b=map(int, x.split())
-	score += SCORE[a+b]
-
-print('RPS Score ='score)
